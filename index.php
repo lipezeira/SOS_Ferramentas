58c07817b510aa2e8e2d9edf51755011f6c51f30
Felipe Ribeiro De Souza <felipemecanicosouza@gmail.com>
2023-11-30 14:35:59 -0300

    Feitos segundo Video de Thiago

--- index.php
+++ index.php
@@ -2,3 +2,19 @@
 
 declare(strict_types=1);
 require __DIR__ . "/vendor/autoload.php";
+
+$url = $_GET['url'] ?? "/";
+
+switch($url){
+    case "/":
+        echo "Página Inicial";
+        break;
+    case "login":
+        echo "Página de login";
+        break;
+    case "cadastro":
+        echo "Página de cadastro";
+        break;
+    default:
+        echo "404 - Página não encontrada!";
+}
