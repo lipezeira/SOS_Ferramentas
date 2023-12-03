c0ac46f6c92617ac4057f381e56817016850e102
lauanda1327 <lauanda.nobre@gmail.com>
2023-12-03 18:56:47 -0300

    atualizacao

--- index.php
+++ index.php
@@ -20,6 +20,10 @@ function componente(string $componente){
     require PASTA_VIEW."componentes/{$componente}.view.php";
 } 
 
+function linkrota($rota = "")
+{
+    return URL_BASE."{$rota}";
+}
 
 
 $url = $_GET['url'] ?? "";
