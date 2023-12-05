beb86a5cca052352058499852203f3048070f5b8
LucianeMeira <lucianemeira205@gmail.com>
2023-12-04 22:17:07 -0300

    Update rotas.php

--- app/rotas.php
+++ app/rotas.php
@@ -6,12 +6,12 @@ Router::add('/','HomeController','index');
 Router::add('/login','LoginController','login');
 Router::add('/teste','LoginController','teste');
 
-Router::add('/teste','HomeController','teste');
+/*Router::add('/teste','HomeController','teste');
 Router::add('/teste2','HomeController','teste2');
 Router::add('/teste3','HomeController','teste3');
 Router::add('/teste4','HomeController','teste4');
 Router::add('/teste5','HomeController','teste5');
-Router::add('/teste6','HomeController','teste6');
+Router::add('/teste6','HomeController','teste6');*/
 
 Router::add('/cadastro','LoginController','criarconta');
 Router::add('/produto','ProdutoController','produto');
